441f142b50f0c1ad5276bd92b66dfc01f492b0ff
lelb <lelb@users.noreply.github.com>
2020-04-19 23:36:38 +0300

    updates
    vue create

---
+++ dist/precache-manifest.8dd87714d264970ad449e73237e56376.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "621b90c7e2f3fd1f2880",
+    "url": "/js/chunk-vendors.09bf74f8.js"
+  },
+  {
+    "revision": "92178dc42ed0002668b4",
+    "url": "/js/app.d42c5ad7.js"
+  },
+  {
+    "revision": "3f11393013cb3e0489715120f59a6a02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c08923414ada761c4d56d4927f4d1d9",
+    "url": "/img/logo.0c089234.svg"
+  },
+  {
+    "revision": "92178dc42ed0002668b4",
+    "url": "/css/app.91c415bd.css"
+  }
+];
--- src/main.js
+++ src/main.js
@@ -3,15 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import {store} from "./store/store"
+import {store} from "./store"
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  store: store,
-  components: { App },
-  template: '<App/>'
-})
+  store,
+  render: h => h(App)
+}).$mount('#app')
